13b91cc84e48788d34683c21cfadc967242b3f30
Kuhn Ludovic <ludovic.kuhn@gmail.com>
2015-08-27 12:14:01 +0200

    first commit

---
+++ app/TagHistory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TagHistory extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'dbo.TagHistory';
+}
